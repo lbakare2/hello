b7f7b749c2b845e1e5566bf13177cff774ba10a8
Your Nameclear <Lanre>
2021-08-26 20:54:24 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/*
+*Name: Lanre Bakare
+*Date: August 26 2021
+*This program prints my name
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+    printf("Olanrewaju Bakare");
+    return 0;
+    
+} 
